9cbff2c02c4bd3b590f02993e5b4d0818f3425fa
samgos <samuel.jj.gosling@gmail.com>
2020-07-16 17:23:00 +0100

    Updates

---
+++ precache-manifest.d2768ab7ea92dfc85b9e154d1a27f741.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd2b814fdef0c4772c375083b50769a2",
+    "url": "/static/media/discord.dd2b814f.png"
+  },
+  {
+    "revision": "f7b159dfa68e8ee4a24e",
+    "url": "/static/css/main.78cc8dd3.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2b9d10205343cd5ca5ff10e54faea6b1",
+    "url": "/static/media/sf-weight.2b9d1020.otf"
+  },
+  {
+    "revision": "ebfc584a7d7bcc704ba2",
+    "url": "/static/js/2.029e8bd4.chunk.js"
+  },
+  {
+    "revision": "0da54ec34c24be2bdf9095f07aa1482a",
+    "url": "/static/media/vldy.0da54ec3.png"
+  },
+  {
+    "revision": "f7fe74765555bb88f1f54d270d647e9b",
+    "url": "/static/media/telegram.f7fe7476.png"
+  },
+  {
+    "revision": "6163476d4921dbaf83480bfe4690fb4e",
+    "url": "/static/media/facebook.6163476d.png"
+  },
+  {
+    "revision": "6976c96a4793fa60b9d370410bef647e",
+    "url": "/static/media/linkedin.6976c96a.png"
+  },
+  {
+    "revision": "926704bee0b822f6ce700e47653e3b5b",
+    "url": "/static/media/twitter.926704be.png"
+  },
+  {
+    "revision": "f7b159dfa68e8ee4a24e",
+    "url": "/static/js/main.4b2e5ddc.chunk.js"
+  },
+  {
+    "revision": "3812695b3db592fc9dba6ad36d681c61",
+    "url": "/static/media/reddit.3812695b.png"
+  },
+  {
+    "revision": "1a9ade6ddd9f07da1d64555fdd48fea3",
+    "url": "/static/media/bct.1a9ade6d.png"
+  },
+  {
+    "revision": "3209ac08268db8ce0e6691207f9388a6",
+    "url": "/static/media/PZ_Raleigh.3209ac08.woff"
+  },
+  {
+    "revision": "e566b498f01951b7b0c771a84aac8c77",
+    "url": "/static/media/sf-italic.e566b498.otf"
+  },
+  {
+    "revision": "873f3070adbe8fdb682d60db515c5d3b",
+    "url": "/static/media/sf-bold-italic.873f3070.otf"
+  },
+  {
+    "revision": "452c02174ae78a92531e5a66fada9a88",
+    "url": "/static/media/sf.452c0217.otf"
+  },
+  {
+    "revision": "c6cea50e70b7a16f956b20eb8dea0f14",
+    "url": "/static/media/sf-bold.c6cea50e.otf"
+  },
+  {
+    "revision": "ebfc584a7d7bcc704ba2",
+    "url": "/static/css/2.8e5f5a8a.chunk.css"
+  },
+  {
+    "revision": "0cd3b28ba32055a18a2b6509744201b4",
+    "url": "/index.html"
+  }
+];
